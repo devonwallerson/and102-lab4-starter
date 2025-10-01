49b461bc07827e4a60792a4cf86cf59fbb23c9a2
devon <gtrgodzilla05@devons-MacBook-Pro.local>
2025-10-01 12:26:09 -0400

    First

--- app/src/main/java/com/codepath/campgrounds/Campground.kt
+++ app/src/main/java/com/codepath/campgrounds/Campground.kt
@@ -4,7 +4,12 @@ import android.support.annotation.Keep
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// TODO: Create a data class for the Data Response
+@Keep
+@Serializable
+data class CampgroundResponse(
+    @SerialName("data")
+    val data: List<Campground>?
+)
 
 
 // TODO: Implement remaining keys of the Campground data class
@@ -13,7 +18,25 @@ import kotlinx.serialization.Serializable
 data class Campground(
     @SerialName("name")
     val name: String?,
+    @SerialName("description")
+    val description: String?,
+    @SerialName("latLong")
+    val latLong: String?,
+    @SerialName("images")
+    val images: List<CampgroundImage>?
+) : java.io.Serializable {
+    // Convenience property to easily get the first image URL if it exists
+    val imageUrl: String
+        get() = images?.firstOrNull { !it.url.isNullOrEmpty() }?.url ?: ""
+}
+
+
+
+@Keep
+@Serializable
+data class CampgroundImage(
+    @SerialName("url")
+    val url: String?,
+    @SerialName("title")
+    val title: String?
 ) : java.io.Serializable
-
-
-// TODO: Create a data class for the Image Response
